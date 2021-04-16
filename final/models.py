aae4360f50636b7daa13ce98eaba2d407df0c145
lopsanglama-hck <np03a190063@heraldcollege.edu.np>
2021-04-16 11:49:11 +0600

    create model Tax

--- final/models.py
+++ final/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tax(models.Model):
+    salary = models.IntegerField()
+    allowance = models.IntegerField()
+    bonus = models.IntegerField()
+    others = models.IntegerField()
